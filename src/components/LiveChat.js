44482baa0c713e91ea0f056e93de9381fd34dcc7
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-05-07 19:45:13 +0530

    New commit

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -37,7 +37,7 @@ const LiveChat = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(addChat({
-            name: 'Krutika Jichkar',
+            name: 'Pranav Kumar',
             message: liveMessage
         }));
         setLiveMessage('');
