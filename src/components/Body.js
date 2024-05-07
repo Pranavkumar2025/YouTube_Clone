a43cbf5dc15d54f580798b9fedec901e3312a9d9
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-05-07 15:31:26 +0530

    New Commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-import { Sidebar } from './Sidebar';
+import { Sidebar } from '../components/Sidebar';
+
 
 const Body = () => {
   return (
